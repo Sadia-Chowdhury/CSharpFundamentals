728d4569da6c7c8707df8373bd684a5a5751e0c9
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-21 14:31:29 +0600

    Even Odd Counter On an Array

---
+++ EvenOddCounter/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EvenOddCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] str = Console.ReadLine().Split();
+            int[] arr;
+            arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(str[i]);
+            }
+            int countEven = 0;
+            int countOdd = 0;
+            foreach (int i in arr)
+            {
+                if (i % 2 == 0)
+                {
+                    countEven++;
+                }
+                else
+                {
+                    countOdd++;
+                }
+            }
+
+            Console.WriteLine("Even Count: " + countEven);
+            Console.WriteLine("Odd Count: " + countOdd);
+
+        }
+    }
+}
