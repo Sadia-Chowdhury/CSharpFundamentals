258b5e76aef7d14ce041de1ae906e9f4aea6a454
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-21 14:46:01 +0600

    Count Words in a string

---
+++ CountWordsInString/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CountWordsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine().Trim();
+            Console.WriteLine(input);
+            string[] str = input.Split(' ');
+
+            Console.WriteLine(str.Length);
+        }
+    }
+}
