2b84ebfc88c8971d6940de3c4e4c1ac806c1d2bb
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-22 23:14:32 +0600

    Frequency Count of letters in a string

---
+++ FrequencyCount/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FrequencyCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Trim();
+            input = input.ToLower();
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    continue;
+                }
+                if(counts.ContainsKey(c))
+                {
+                    counts[c]++;
+                }
+                else
+                {
+                    counts[c] = 1;
+                }
+            }
+
+            int max = 0;
+            char maxc=' ';
+            foreach(var pair in counts)
+            {
+                if(pair.Value >= max)
+                {
+                    max = pair.Value; 
+                    maxc = pair.Key;    
+                }
+            }
+
+            Console.WriteLine("Most Frequent character "+ maxc +". And it apeared "+ max +"times.");
+        }
+    }
+}
