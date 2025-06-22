186512f3ef43da8a6dede3d7a156f3004e8a0d2b
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-23 00:51:23 +0600

    Dynamic Array List

---
+++ DynamicArrayList/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DynamicArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            string[] numbers = input.Split();
+
+            for(int i=0;i<n;i++)
+            {
+                list.Add(Convert.ToInt32(numbers[i]));
+            }
+
+            list.Insert(2, 10);
+            list.RemoveAt(4);
+            list.Insert(0, 99);
+            
+            //99 1 2 10 4 5
+
+            foreach(int i in list)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
