33b54beeea9866ea34e7719b1064c4a28c1b29cd
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-23 00:04:56 +0600

    two Sum Using Dictionary

---
+++ TwoSumUsingDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TwoSumUsingDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =Convert.ToInt32(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            string[] str = input.Split(' ');
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(str[i]);
+            }
+            int target = Convert.ToInt32(Console.ReadLine());
+            
+            Dictionary<int,int> map = new Dictionary<int,int>();
+
+            for (int i = 0; i < n; i++)
+            {
+               int sum = target - arr[i];
+               if(map.ContainsKey(sum))
+                {
+                    Console.WriteLine("Pair Found at indices {0} and {1}.", map[sum],i);
+                }
+               else
+                {
+                    map[arr[i]]= i;
+                }
+            }
+
+        }
+       
+    }
+}
