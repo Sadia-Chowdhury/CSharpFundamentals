5c0082030153de70ebed1ec77a16326e6d9e3255
Sadia-Chowdhury <sadiachowdhury266@gmail.com>
2025-06-23 23:36:04 +0600

    fidning even numbers in a list using LINQ

---
+++ EvenNumberInAListUsingLINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EvenNumberInAListUsingLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1,2,3,4,54,6,7,8,9,2,4,4342,56,54,566,12};
+
+           List<int>evenList= list.Where(x => x % 2 == 0).ToList();
+
+            foreach(int x in evenList)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
